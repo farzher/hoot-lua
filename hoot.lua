774f63e3a2b0e6aba8bef2c618ce4d663e70c830
Stephen Kamenar <stephenkamenar@gmail.com>
2016-01-16 05:37:18 -0500

    refactor for performance optimization

--- hoot.lua
+++ hoot.lua
@@ -3,76 +3,96 @@ local function table_import(t, t2)
 end
 
 
-local methods = {}
-function methods:get(key) return self.timers[key] end
-function methods:clear(key) self.timers[key] = nil end
-function methods:destroy() self.instances[self.t] = nil end
-function methods:set(f, delay, options)
-  local key
-
-  -- If f is string, default key to f
-    if type(f)=='string' then key = f end
-
-  -- options
-    if options then
-      if options.key~=nil then key = options.key end
-
-      if options.ifactive=='nop' then
-        if self.timers[key] and self.timers[key].delay>0 then return end
-      end
-    end
-
-  -- Pick a unqiue random key
-    if not key then
-      repeat
-        key = math.random()
-      until self.timers[key]==nil
-    end
-
-  self.timers[key] = {delay=delay, f=f, options=options}
-
-  return key
-end
-
-
-
 local function factory()
-  local hoot = {}
-  hoot.instances = {}
-  setmetatable(hoot, {
-    __call = function(self, t)
-      if self.instances[t] then return self.instances[t] end
+  -- Our new hoot object
+    local hoot = {}
+  -- When you call hoot(self) self gets shoved in this list as the key with a wrapped version as the value
+    hoot.wrapped_list = {}
+  -- For iteration performance, all timers from all objects also exist here
+  -- (This means they need to be created and cleared in pairs /:)
+    hoot.timer_set = {}
 
-      local instance = {t=t, timers={}}
-      table_import(instance, methods)
-      self.instances[t] = instance
-      return instance
+
+  -- Wrapper class (in factory for hoot closure!)
+    local Wrapper = {}
+    function Wrapper:get(key) return self.timers[key] end
+    function Wrapper:clear(key)
+      local timer = self.timers[key]
+      if not timer then return end
+      self.timers[key] = nil
+      hoot.timer_set[timer] = nil
+      return true
+    end
+    function Wrapper:destroy()
+      for _, timer in pairs(self.timers) do hoot.timer_set[timer] = nil end
+      hoot.wrapped_list[self.original] = nil
+    end
+    function Wrapper:set(f, delay, options)
+      local key
+
+      -- If f is string, default key to f
+        if type(f)=='string' then key = f end
+
+      -- options
+        if options then
+          if options.key~=nil then key = options.key end
+
+          if options.ifactive=='nop' then
+            if self.timers[key] and self.timers[key].delay>0 then return end
+          end
+        end
+
+      -- Pick a unqiue random key
+        if not key then
+          repeat
+            key = math.random()
+          until self.timers[key]==nil
+        end
+
+      local timer = {wrapped=self, key=key, delay=delay, f=f, options=options}
+      self.timers[key] = timer
+      hoot.timer_set[timer] = true
+
+      return key
+    end
+
+
+  setmetatable(hoot, {
+    __call = function(self, original)
+      if self.wrapped_list[original] then return self.wrapped_list[original] end
+
+      local wrapped = {original=original, timers={}}
+      table_import(wrapped, Wrapper)
+      self.wrapped_list[original] = wrapped
+      return wrapped
     end;
   })
+
+
   function hoot.update(dt)
-    for _, instance in pairs(hoot.instances) do
-      for key, timer in pairs(instance.timers) do
-        if timer.delay>0 then
-          timer.delay = timer.delay - dt
+    for timer in pairs(hoot.timer_set) do
+      if timer.delay>0 then
+        timer.delay = timer.delay - dt
 
-          if timer.options and timer.options.update then
-            timer.options.update(instance.t)
+        local original = timer.wrapped.original
+
+        if timer.options and timer.options.update then
+          timer.options.update(original)
+        end
+
+        if timer.delay<=0 then
+          if type(timer.f)=='string' then
+            if original[timer.f] then original[timer.f](original) end
+          elseif type(timer.f)=='function' then
+            timer.f(original)
           end
 
-          if timer.delay<=0 then
-            if type(timer.f)=='string' then
-              if instance.t[timer.f] then instance.t[timer.f](instance.t) end
-            else
-              timer.f(instance.t)
-            end
-
-            instance.timers[key] = nil
-          end
+          timer.wrapped:clear(timer.key)
         end
       end
     end
   end
-  function hoot.destroy() hoot.instances = {} end
+  function hoot.destroy() hoot.wrapped_list = {} end
   function hoot.new() return factory() end
 
   return hoot
